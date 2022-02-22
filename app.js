57a8c09c06f113f09994836c2ab86ec4db981549
Thomas Flanigan <tflanig1@gmail.com>
2022-02-21 22:15:12 -0600

    Added TODO

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const request = require('request');
 
 // TODO: Remove test code below
+// TODO: Fine as a job for now, but should run server code later to run as svc.
 request('https://dojour.us/api/', { json: true }, (err, res, body) => {
   if (err) { return console.log(err); }
   console.log(body.url);
